0c138f8e8086517d06374fb8acd1d8636db30f6e
Carl Daniel B. Runas <carldanielrunas01@gmail.com>
2024-09-13 11:51:47 +0800

    Git-practice commit

---
+++ app.js
@@ -0,0 +1,16 @@
+let firstFavNum = 21
+let secondFavNum = 12
+
+console.log("Greater than:", firstFavNum > secondFavNum);
+console.log("Less than:",firstFavNum < secondFavNum);
+console.log("Greater than or equal to:", firstFavNum >= secondFavNum);
+console.log("Less than or equal to:", firstFavNum <= secondFavNum);
+console.log("Stict equality (===):", firstFavNum === secondFavNum);
+console.log("Loose equality (==):", firstFavNum == secondFavNum);
+console.log("Strict inequality (!==):", firstFavNum !== secondFavNum);
+console.log("Loose inequality (!=):", firstFavNum != secondFavNum);
+
+console.log("AND:",(firstFavNum > 5) && (secondFavNum < 5 ));
+console.log("OR:",(firstFavNim > 5) || (secondFavNum > 5));
+console.log("NOT:",!(firstFavNum === secondFavNum))!
+
---
+++ comparison.js
@@ -0,0 +1,15 @@
+let firstFavNum = 21;
+let secondFavNum = 12;
+
+console.log("Greater than:", firstFavNum > secondFavNum);
+console.log("Less than:", firstFavNum < secondFavNum);
+console.log("Greater than or equal to:", firstFavNum >= secondFavNum);
+console.log("Less than or equal to:", firstFavNum <= secondFavNum);
+console.log("Strict iquality (===):", firstFavNum === secondFavNum);
+console.log("Loose iquality (==):", firsyFavNum == secondFavNum);
+console.log("Strict inquality (!==):", firstFavNum !== secondFavNum);
+console.log("Loose inequality (!=):", firstFavNum != secondFavNum);
+
+console.log("AND:",(firstFavNum > 5) && (secondFavNum < 5));
+console.log("OR:",(firstFavNum > 5) ||(secondFavNum < 5));
+console.log("NOT:",!(firstFavNum === secondFavNum))!
