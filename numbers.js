88bb0635e00f29aa7bb577bccdd2bed7cf78d4d4
Carl Daniel B. Runas <carldanielrunas01@gmail.com>
2024-09-13 10:32:04 +0800

    Git-practice commit

---
+++ numbers.js
@@ -0,0 +1,16 @@
+let firstFavNum = 21;
+let secondFavNum = 12;
+
+let addition = firstFavNum + secondFavNum;
+let substraction = firstFavNum - secondFavNum;
+let multiplication = firstFavNum * secondFavNum;
+let division = firstFavNum / secondFavNum;
+let modulus = firstFavNum % secondFavNum;
+let exponentiation = Math.pow(firstFavNum, secondFavnum);
+
+console.log("Addition:", addition);
+console.log("Substaction:",substraction);
+console.log("Multiplication:",multiplication);
+console.log("Division:",division);
+console.log("Modulus:",modulus);
+console.log("Eponentiation:", exponentiation);
